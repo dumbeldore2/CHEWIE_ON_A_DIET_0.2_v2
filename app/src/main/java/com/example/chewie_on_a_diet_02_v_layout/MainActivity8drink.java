90016ec639a14b2e8a68d1a5e36ffaf059f4a652
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-01-29 23:48:20 +0100

    MainActivity8drink.java is up to date

--- app/src/main/java/com/example/chewie_on_a_diet_02_v_layout/MainActivity8drink.java
+++ app/src/main/java/com/example/chewie_on_a_diet_02_v_layout/MainActivity8drink.java
@@ -20,13 +20,14 @@ import android.widget.TextView;
 
 public class MainActivity8drink extends AppCompatActivity {
 
-    ImageView imageView1;
+    ImageView imageView1,imageView2;
 
-    TextView textView1,textView2,textView3;
+    TextView textView1,textView2,textView3,textView4;
 
     ListView listView;
 
     MainActivity8drinkBar mainActivity8drinkBar;
+
     String data[] = {"naam = appel","naam = peer","naam = olifant","naam = muis"};
     String data2[] = {"maker = appelman","maker = peerman","maker = olifantman","maker = muisman"};
     String data3[] = {"aantal calories/100ml = 1500","aantal calories/100ml = 1500","aantal calories/100ml = 1500","aantal calories/100ml = 1500"};
@@ -41,10 +42,12 @@ public class MainActivity8drink extends AppCompatActivity {
         );
 
         imageView1 = findViewById(R.id.image_1);
+        imageView2 = findViewById(R.id.image_2);
 
         textView1 = findViewById(R.id.text_1);
         textView2 = findViewById(R.id.text_2);
         textView3 = findViewById(R.id.text_3);
+        textView4 = findViewById(R.id.text_4);
 
         listView = findViewById(R.id.listview_1);
 
@@ -53,6 +56,7 @@ public class MainActivity8drink extends AppCompatActivity {
 
 
         click_fun_1();
+        click_fun_2();
 
         // setColorToGradiant(textView1);
         // setColorToGradiant(textView2);
@@ -102,6 +106,41 @@ public class MainActivity8drink extends AppCompatActivity {
                 startActivity(intent,options.toBundle());
             }
         });
+    }
+
+    public void click_fun_2(){
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity13makedrinkobject.class);
+
+                Pair[] pairs = new Pair[20];
+                pairs[0] = new Pair<View,String>(textView4,"1");
+                pairs[1] = new Pair<View,String>(textView4,"2");
+                pairs[2] = new Pair<View,String>(textView4,"3");
+                pairs[3] = new Pair<View,String>(textView4,"4");
+                pairs[4] = new Pair<View,String>(textView4,"5");
+                pairs[5] = new Pair<View,String>(textView4,"6");
+                pairs[6] = new Pair<View,String>(textView4,"7");
+                pairs[7] = new Pair<View,String>(textView4,"8");
+                pairs[8] = new Pair<View,String>(textView4,"9");
+                pairs[9] = new Pair<View,String>(textView4,"10");
+                pairs[10] = new Pair<View,String>(textView4,"11");
+                pairs[11] = new Pair<View,String>(textView4,"12");
+                pairs[12] = new Pair<View,String>(textView4,"13");
+                pairs[13] = new Pair<View,String>(textView4,"14");
+                pairs[14] = new Pair<View,String>(textView4,"15");
+                pairs[15] = new Pair<View,String>(textView4,"16");
+                pairs[16] = new Pair<View,String>(textView4,"17");
+                pairs[17] = new Pair<View,String>(textView4,"18");
+                pairs[18] = new Pair<View,String>(textView4,"19");
+                pairs[19] = new Pair<View,String>(textView4,"20");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity8drink.this, pairs);
+
+                startActivity(intent,options.toBundle());
+            }
+        }); 
     }
     private void setColorToGradiant(TextView textView){
         TextPaint textPaint = textView.getPaint();
